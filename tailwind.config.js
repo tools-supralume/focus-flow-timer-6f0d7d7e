6803bffee395305057bec0ba3c485e56a6b7f5d3
tools-supralume <tools@supralume.com>
2026-01-30 19:39:31 -0500

    Add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      animation: {
+        'pulse-slow': 'pulse 3s infinite',
+        'spin-slow': 'spin 20s linear infinite',
+      }
+    },
+  },
+  plugins: [],
+}
